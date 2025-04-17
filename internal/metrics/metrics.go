debc625649ddfd3ba406ded486e7137c1b8575ec
Niahh <samy.ouastani@gmail.com>
2025-04-17 16:47:31 +0200

    feat: adding nas support for exposed metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,6 +2,7 @@
 package metrics
 
 import (
+	"github.com/free5gc/amf/internal/metrics/nas"
 	"github.com/free5gc/amf/pkg/factory"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
@@ -11,11 +12,13 @@ import (
 func Init(cfg *factory.Config) *prometheus.Registry {
 	reg := prometheus.NewRegistry()
 
+	namespace := cfg.GetMetricsNamespace()
 	reg.Unregister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 
 	var amfMetrics []prometheus.Collector
 
 	// Append here the collector you want to register to the prometheus registry
+	amfMetrics = append(amfMetrics, nas.GetNasHandlerMetrics(namespace)...)
 
 	initMetric(amfMetrics, reg)
 
